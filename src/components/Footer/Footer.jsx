fcde50d649f79aefae13f434a793e6c7f6116e2b
Mani <mnmnkandan@gmail.com>
2025-06-26 16:36:40 +0530

    modification

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -108,7 +108,7 @@ const Footer = () => {
               <div className="mt-6">
                 <div className="flex items-center gap-3">
                   <FaLocationArrow />
-                  <p>Noida, Uttar Pradesh</p>
+                  <p>Trichy , TamilNadu</p>
                 </div>
                 <div className="flex items-center gap-3 mt-3">
                   <FaMobileAlt />
